0c6a2ea1c79fe77000d51ad34f2f50b9e435a7ab
zhuanghaixin <649308293@qq.com>
2019-08-18 17:25:23 +0800

    [vue-admin-dashboard]完成登录界面样式

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+import SignIn from "./views/SignInFlow/SignIn.vue";
+import Request from "./views/SignInFlow/Request.vue";
+import Recover from "./views/SignInFlow/Recover.vue";
 
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
-  routes: [
-    {
+  routes: [{
       path: "/",
       name: "home",
       component: Home
@@ -16,11 +19,22 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: About
+    },
+    {
+      path: "/signin",
+      name: "SignIn",
+      component: SignIn
+    },
+    {
+      path: "/request",
+      name: "Request",
+      component: Request
+    },
+    {
+      path: "/recover",
+      name: "Recover",
+      component: Recover
     }
   ]
 });
---
+++ src/views/SignInFlow/SignIn.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="container">
+    <div class="request">
+      Don't have a Design+Code HQ account
+      <router-link to="/request">Request an account</router-link>
+    </div>
+    <div class="login">
+      <img src="@/assets/DCHQ.svg" alt />
+      <h4>Sign into Design+Code HQ</h4>
+      <input type="email" name id placeholder="Email" />
+      <input type="password" name id placeholder="Password" />
+      <button>Sign in</button>
+      <router-link to="/recover">Forgotten your password?</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "sigin"
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  min-height: 100vh;
+}
+.login {
+  width: 400px;
+}
+.request {
+  position: absolute;
+  top: 40px;
+  right: 40px;
+  color: rgba(255, 255, 255, 0.3);
+  a {
+    color: white;
+  }
+}
+h4 {
+  margin: 0;
+  font-family: SF Pro Display;
+  font-size: 24px;
+  line-height: 34px;
+  color: #ffffff;
+}
+input {
+  height: 60px;
+  width: 100%;
+  font-size: 20px;
+  background: rgba(255, 255, 255, 0.2);
+  border: 1px solid rgba(255, 255, 255, 0.2);
+  box-sizing: border-box;
+  border-radius: 4px;
+  color: #ffffff;
+  padding-left: 20px;
+  margin-top: 20px;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.3);
+  }
+}
+button {
+  /* teal */
+  background: #56ccf2;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+  border-radius: 4px;
+  height: 60px;
+  width: 100%;
+  box-sizing: border-box;
+  margin-top: 20px;
+  margin-bottom: 40px;
+  font-family: SF Pro Text;
+  font-size: 20px;
+  line-height: 24px;
+  color: #ffffff;
+}
+a {
+  font-family: SF Pro Text;
+  font-size: 16px;
+  line-height: 25px;
+  color: rgba(255, 255, 255, 0.3);
+  text-decoration: none;
+}
+</style>
