2e7302bcee6afcb7f3d64e8cea4273ca4766fab6
zhuanghaixin <649308293@qq.com>
2019-08-27 18:43:15 +0800

    [vue-admin-dashboard] 修复bug

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import * as netlifyIdentityWidget from "netlify-identity-widget";
 Vue.config.productionTip = false;
-
+// initialize Netlify Identity
+netlifyIdentityWidget.init()
 new Vue({
   router,
   store,
--- src/router.js
+++ src/router.js
@@ -5,21 +5,28 @@ import Team from "./views/Team.vue";
 import SignIn from "./views/SignInFlow/SignIn.vue";
 import Request from "./views/SignInFlow/Request.vue";
 import Recover from "./views/SignInFlow/Recover.vue";
+import * as netlifyIdentityWidget from "netlify-identity-widget";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [{
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      meta: {
+        requireAuth: true
+      }
     },
     {
       path: "/team",
       name: "team",
-      component: Team
+      component: Team,
+      meta: {
+        requireAuth: true
+      }
     },
     {
       path: "/signin",
@@ -38,3 +45,16 @@ export default new Router({
     }
   ]
 });
+// router.beforeEach((to, from, next) => {
+//   // ...
+//   const cuurentUser = netlifyIdentityWidget.currentUser();
+//   const requireAuth = to.matched.some(record => {
+//     return record.meta.requireAuth
+//   })
+//   if (requireAuth && !cuurentUser) {
+//     next("signin")
+//   } else {
+//     next()
+//   }
+// })
+export default router;
