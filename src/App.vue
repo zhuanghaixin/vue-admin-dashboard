a1b370d77dc2b7a3fad7bac508823b50ca04a9d8
zhuanghaixin <649308293@qq.com>
2019-08-27 17:32:22 +0800

    [vue-admin-dashboard] 修改标题居中

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ p {
   font-family: system-font-family;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  // text-align: center;
+  text-align: center;
   color: $white;
 }
 #nav {
